aeaee69a0785fa98e908cec9b6be24ab42f69f8c
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-09-14 07:55:27 -0300

    Create Type Page

---
+++ frontend/src/pages/Type.vue
@@ -0,0 +1,16 @@
+<template>
+  <Main>
+    <TypeTable></TypeTable>
+  </Main>
+</template>
+
+<script>
+import Main from "../components/Main.vue";
+
+import TypeTable from "../components/TypeTable.vue";
+
+export default {
+  name: "Financing",
+  components: { Main, TypeTable },
+};
+</script>
