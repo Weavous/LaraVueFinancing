f89d7a1e767adf182b93e250d2422fd921b0569f
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-09-28 14:11:02 -0300

    Improve Financing Page Component

---
+++ frontend/src/components/EntryInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="mb-5">
+    <label class="block text-gray-700"> Entry value </label>
+    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" @keyup="change()" v-model="entry" v-money="money">
+  </div>
+</template>
+
+<script>
+import { VMoney } from "v-money";
+
+export default {
+  data() {
+    return {
+      entry: 0,
+      money: {
+        decimal: ",",
+        thousands: ".",
+        prefix: "R$ ",
+        precision: 2,
+        masked: false
+      }
+    };
+  },
+  directives: { money: VMoney },
+  methods: {
+    change() {
+      this.$emit("change", {
+        entry: this.entry,
+        timestamp: new Date()
+      });
+    }
+  }
+};
+</script>
--- frontend/src/pages/Financing.vue
+++ frontend/src/pages/Financing.vue
@@ -2,38 +2,16 @@
   <Main>
     <div class="flex justify-center">
       <div class="flex flex-col">
-        <div class="inline-block relative w-64">
-          <span class="text-gray-700">Selecione um veículo</span>
+        <VehiclePicker @change="change"></VehiclePicker>
 
-          <select @change="changePrice()" v-model="vehicles.name" class="block appearance-none w-full bg-white border mb-5 border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-            <option v-for="vehicle in vehicles" :key="vehicle.id">{{ vehicle.name }}</option>
-          </select>
-
-          <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"></path>
-            </svg>
-          </div>
-        </div>
-
-        <div class="mb-5">
-          <span class="text-gray-700">Preço:</span>
-          <span>{{ vehicles.price }}</span>
-        </div>
-
-        <div class="mb-5">
-          <div>
-            <label class="block text-gray-700"> Valor de entrada </label>
-            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" v-model="price" v-money="money">
-          </div>
-        </div>
+        <EntryInput @change="entrychange"></EntryInput>
 
         <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" @click="getConditions()">
-          Simular
+          Simulation
         </button>
 
         <div class="mb-5">
-          <span class="text-gray-700">Planos</span>
+          <span class="text-gray-700">Plans</span>
         </div>
 
         <div class="mb-5">
@@ -46,90 +24,64 @@
 
 <script>
 import Main from "../components/Main.vue";
-
-import vehicles from "../services/vehicles.js";
-
-import { VMoney } from "v-money";
+import VehiclePicker from "../components/VehiclePicker.vue";
+import EntryInput from "../components/EntryInput.vue";
+import financing from "../services/financing.js";
 
 export default {
   name: "Financing",
-  components: { Main },
+  components: { Main, VehiclePicker, EntryInput },
   data() {
     return {
-      vehicles: {
-        name,
-        price: 0,
-        object: {},
-        data: []
-      },
-      financing: {
-        id: 0
-      },
+      vehicle: {},
       plans: [],
-      payment: 0,
-      price: 0.0,
-      money: {
-        decimal: ",",
-        thousands: ".",
-        prefix: "R$ ",
-        precision: 2,
-        masked: false
-      }
+      entry: 0
     };
   },
-  directives: { money: VMoney },
   methods: {
     getConditions() {
-      const vehicle = this.vehicles.object;
-      const entry = this.price.replace(/\D+/g, "") / 100;
+      const { id, price } = { ...this.vehicle };
 
-      if (typeof vehicle === typeof {}) {
-        const price = vehicle.price / 1000;
-        const models = [
-          { id: 1, quantity: 48 },
-          { id: 1, quantity: 12 },
-          { id: 1, quantity: 6 }
-        ];
-
-        const plans = models.map((model, key) => {
-          const money = this.toMoney((price - entry) / model.quantity);
-
-          return {
-            id: key,
-            price: this.toMoney(price),
-            quantity: model.quantity,
-            parcel: money,
-            text: `${model.quantity}x de ${money}`
-          };
-        });
-
-        this.plans = plans;
-      }
-    },
-    changePrice() {
-      const vehicle = this.vehicles.find(
-        vehicle => vehicle.name === this.vehicles.name
-      );
-
-      if (typeof undefined === typeof vehicle) {
+      if (id === 0 || id === undefined) {
         return false;
       }
 
-      this.vehicles.object = vehicle;
-      this.vehicles.price = this.toMoney(vehicle.price);
+      this.plans = this.getPlans(price);
+    },
+    change(evt) {
+      this.vehicle = evt.el;
+    },
+    onlyIntegers(string) {
+      return toString(string).replace(/\D+/g, "");
+    },
+    centsToDollar(cents) {
+      return cents / 100;
     },
     toMoney(money) {
-      return new Intl.NumberFormat("en-US", {
+      const options = {
         style: "currency",
         currency: "USD",
         currencyDisplay: "narrowSymbol"
-      }).format(money);
+      };
+
+      return new Intl.NumberFormat("en-US", options).format(money);
+    },
+    entrychange(evt) {
+      this.entry = evt.entry;
+    },
+    getPlans(price) {
+      return financing.index().map((model, key) => {
+        const money = this.toMoney((this.centsToDollar(price) - this.onlyIntegers(this.entry) / 100) / model.quantity);
+
+        return {
+          id: key,
+          price: this.toMoney(this.centsToDollar(price)),
+          quantity: model.quantity,
+          parcel: money,
+          text: `${model.quantity}x of ${money}`
+        };
+      });
     }
-  },
-  created() {
-    vehicles.index().then(response => {
-      this.vehicles = response.data;
-    });
   }
 };
 </script>
