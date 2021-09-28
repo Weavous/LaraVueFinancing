eef6c216e36a17d486aded17f024152ded1a6407
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-09-28 14:11:58 -0300

    Begin Financing Service Development

--- frontend/src/services/financing.js
+++ frontend/src/services/financing.js
@@ -1,5 +1,14 @@
-import { http } from './http.js';
+import { http } from "./http.js";
 
 export default {
-    exchanges: (token, data) => { return http.post(`/exchanges`, { 'token': token, ...data }) }
+  exchanges: (token, data) => {
+    return http.post(`/exchanges`, { token: token, ...data });
+  },
+  index: () => {
+    return [
+      { id: 1, quantity: 48 },
+      { id: 1, quantity: 12 },
+      { id: 1, quantity: 6 }
+    ];
+  }
 };
